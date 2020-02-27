7f0e16614d848959ecb43aa7eb21cdab74cff3bd
MikhailMe <neon-pf@yandex.ru>
2020-02-28 02:18:03 +0300

    added tasks and restore needed files

--- bla/Source.cpp
+++ bla/Source.cpp
@@ -1,17 +1,11 @@
 #include <iostream>
-#include "Car.h"
-
+#include <string>
+#include "Tasks.h"
 
 int main()
 {
-	Car volvo = Car();
-	volvo.color = "orange";
-	volvo.setCoef(30);
-	std::cout << volvo.color << std::endl;
-	std::cout << volvo.getColorOfGlass() << " " << volvo.getCoef() << std::endl;
-	/*std::string str = volvo.drive("aaaa");
-	std::cout << str << std::endl;
-	std::cout << volvo.getSpeed() << std::endl;*/
+	int result = sqr(5);
+	std::cout << result << std::endl;
 	system("pause");
 	return 0;
 }
